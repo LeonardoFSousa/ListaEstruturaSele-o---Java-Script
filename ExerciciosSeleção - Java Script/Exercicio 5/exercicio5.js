efeccccef0fb2c472f66c57350f412cbf97c815f
LeonardoFSousa <130254546+LeonardoFSousa@users.noreply.github.com>
2023-04-18 21:10:51 -0300

    Add files via upload

---
+++ ExerciciosSeleção - Java Script/Exercicio 5/exercicio5.js
@@ -0,0 +1,34 @@
+function exe5(){
+
+    let n1 = Number (document.getElementById("n1").value)
+    let n2 = Number (document.getElementById("n2").value)
+
+    let opcao = Number (document.getElementById("opcao").value)
+
+    switch(opcao) {
+        case 1: resultado = (n1 + n2) / 2
+        break
+
+        case 2: if (n1 >= n2) {
+            resultado = n1 - n2
+        }
+        else{
+            resultado = n2 - n1
+        }
+        break
+
+        case 3: resultado = n1 * n2
+        break
+
+        case 4: if (n2 != 0){
+            resultado = n1 / n2
+        }
+        else{
+            resultado = "Não existe número dividido por zero"
+        }
+        break
+
+        default: resultado = "Opção Inválida"
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
